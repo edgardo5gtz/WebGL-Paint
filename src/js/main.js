2665d4be2dc82387a3a511fb1f2fa5deb0f6f2bb
Edgardo Gutierrez <edgardo5gtz@gmail.com>
2017-10-28 01:40:15 -0500

    Click function working for color buttons

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,74 @@
 $(function(){
-    console.log("Hello from Three.js")
-    var renderer = THREE.WebGLRenderer();
-    console.log("Hello from main.js and Jquery")
+    var $whiteBtn = $('.white');
+    var $greyBtn = $(".grey");
+    var $blackBtn = $(".black");
+    var $blueBtn = $(".blue");
+    var $yellowBtn = $(".yellow");
+    var $greenBtn = $(".green");
+    var $pinkBtn = $(".pink");
+    var $orangeBtn = $(".orange");
+    var $purpleBtn = $(".purple");
+    var $skyBlueBtn = $(".sky-blue");
+    var $aquaBtn = $(".aqua");
+    var $darkBlueBtn = $(".dark-blue");
+    var $brownBtn = $(".brown");
+    var $redBtn = $(".red");
+
+    $whiteBtn.click(function(){
+        console.log("I'm white button")
+    });
+
+    $greyBtn.click(function() {
+      console.log("I'm grey button");
+    });
+
+    $blackBtn.click(function() {
+      console.log("I'm black button");
+    });
+
+    $blueBtn.click(function() {
+      console.log("I'm blue button");
+    });
+
+    $yellowBtn.click(function() {
+      console.log("I'm yellow button");
+    });
+
+    $greenBtn.click(function() {
+      console.log("I'm green button");
+    });
+
+    $pinkBtn.click(function() {
+      console.log("I'm pink button");
+    });
+
+    $orangeBtn.click(function() {
+      console.log("I'm orange button");
+    });
+
+    $purpleBtn.click(function() {
+      console.log("I'm purple button");
+    });
+
+    $skyBlueBtn.click(function() {
+      console.log("I'm sky-blue button");
+    });
+
+    $aquaBtn.click(function() {
+      console.log("I'm aqua button");
+    });
+
+    $darkBlueBtn.click(function() {
+      console.log("I'm dark-blue button");
+    });
+
+    $brownBtn.click(function() {
+      console.log("I'm brown button");
+    });
+
+    $redBtn.click(function() {
+      console.log("I'm red button");
+    });
+
+    
 });
